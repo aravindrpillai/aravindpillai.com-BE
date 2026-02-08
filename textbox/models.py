35cc4090336749cf8a3e17b3f3728616afc63649
Aravind R Pillai <aravindrpillai1992@gmail.com>
2026-02-08 00:09:09 -0500

    model update

--- textbox/models.py
+++ textbox/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Textbox(models.Model):
 
-    code = models.TextField(null=False, unique=True)
-    content = models.CharField(max_length=10, null=False)
+    content = models.TextField(null=False, unique=True)
+    code = models.CharField(max_length=10, null=False)
     
     class Meta:
         db_table = "textbox"
